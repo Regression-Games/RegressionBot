50b1e6ea1203538b7d5b57e0737142b07f701509
BuildTools <unconfigured@null.spigotmc.org>
2022-11-07 17:13:20 -0500

    do not export bot-samples

--- regression-games/bot-samples/index.js
+++
@@ -1,9 +0,0 @@
-const simpleStrategy = require('./simpleStrategy');
-const intermediateStrategy = require('./intermediateStrategy');
-const advancedStrategy = require('./advancedStrategy');
-
-module.exports = {
-    simpleStrategy,
-    intermediateStrategy,
-    advancedStrategy,
-};
