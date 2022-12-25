01a802bebed21e0530ec9bdc56752dfd9adc4b33
Jasper van Merle <jaspervmerle@gmail.com>
2022-12-25 18:42:17 +0100

    Update JSDoc plugin to remove need for "TypeScript JSDoc:" header

--- docs/remove-typescript-syntax.js
+++ docs/remove-typescript-syntax.js
@@ -3,15 +3,13 @@ exports.handlers = {
    * In order to generate TypeScript definitions from JSDoc comments we need to use TypeScript-specific JSDoc syntax:
    * https://www.typescriptlang.org/docs/handbook/jsdoc-supported-types.html#import-types
    * 
-   * In the JSDoc comments we prefix all blocks of TypeScript-specific syntax with "TypeScript JSDoc:",
-   * which we remove here. This way we can run jsdoc2md without errors, but still use TypeScript-specific syntax
-   * to improve the generated TypeScript definition files.
+   * Here we remove the TypeScript-specific syntax so we can run jsdoc2md without errors.
    *
    * @param {object} e
    * @param {string} e.filename
    * @param {string} e.source
    */
   beforeParse(e) {
-    e.source = e.source.replace(/TypeScript JSDoc:\s*(\* @typedef.*\s*)*/gm, '\n');
+    e.source = e.source.replace(/@typedef\s*{\s*import\(.*}.*$/gm, '');
   },
 };
